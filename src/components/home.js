b32f15bbd67d08eccbb48611a7b6a4be5ba6d24c
amanpurwar <amanpurwar35@gmail.com>
2020-04-17 20:02:35 +0530

    Removed the timeseries graphs and integrated sheets to JSON API

--- src/components/home.js
+++ src/components/home.js
@@ -43,12 +43,14 @@ function Home(props) {
         {data: statesDailyResponse},
         updateLogResponse,
         stateTestResponse,
+        {data: mumbaiWardResponse}
       ] = await Promise.all([
         axios.get('https://api.covid19india.org/data.json'),
         axios.get('https://api.covid19india.org/state_district_wise.json'),
         axios.get('https://api.covid19india.org/states_daily.json'),
         axios.get('https://api.covid19india.org/updatelog/log.json'),
         axios.get('https://api.covid19india.org/state_test_data.json'),
+        axios.get('https://script.google.com/macros/s/AKfycbwEZfKz70mGL1YPW9qBtyx9L3IoLqyhLl46pnGb3kkqIcip2A/exec?id=17jRnQ8hS764Q7yqZ2l2qUIO6LYECWrKLsLUuYl2fYxI&sheet=COVID-19%20Cases')
       ]);
       setStates(response.data.statewise);
       const ts = parseStateTimeseries(statesDailyResponse);
@@ -59,6 +61,7 @@ function Home(props) {
       setStateDistrictWiseData(stateDistrictWiseResponse.data);
       setActivityLog(updateLogResponse.data);
       setFetched(true);
+      console.log(mumbaiWardResponse);
     } catch (err) {
       console.log(err);
     }
@@ -113,19 +116,6 @@ function Home(props) {
               </div>
             </div>
           </div>
-
-          {states.length > 1 && <Level data={states} />}
-          {fetched && <Minigraph timeseries={timeseries['TT']} />}
-          {fetched && (
-            <Table
-              forwardRef={refs[0]}
-              states={states}
-              summary={false}
-              stateDistrictWiseData={stateDistrictWiseData}
-              onHighlightState={onHighlightState}
-              onHighlightDistrict={onHighlightDistrict}
-            />
-          )}
         </div>
 
         <div className="home-right">
@@ -140,90 +130,7 @@ function Home(props) {
                 onMapHighlightChange={onMapHighlightChange}
               />
 
-              <div
-                className="timeseries-header fadeInUp"
-                style={{animationDelay: '2.5s'}}
-                ref={refs[2]}
-              >
-                <h1>Spread Trends</h1>
-                <div className="tabs">
-                  <div
-                    className={`tab ${graphOption === 1 ? 'focused' : ''}`}
-                    onClick={() => {
-                      setGraphOption(1);
-                    }}
-                  >
-                    <h4>Cumulative</h4>
-                  </div>
-                  <div
-                    className={`tab ${graphOption === 2 ? 'focused' : ''}`}
-                    onClick={() => {
-                      setGraphOption(2);
-                    }}
-                  >
-                    <h4>Daily</h4>
-                  </div>
-                </div>
-
-                <div className="scale-modes">
-                  <label>Scale Modes</label>
-                  <div className="timeseries-mode">
-                    <label htmlFor="timeseries-mode">Uniform</label>
-                    <input
-                      type="checkbox"
-                      checked={timeseriesMode}
-                      className="switch"
-                      aria-label="Checked by default to scale uniformly."
-                      onChange={(event) => {
-                        setTimeseriesMode(!timeseriesMode);
-                      }}
-                    />
-                  </div>
-                  <div
-                    className={`timeseries-logmode ${
-                      graphOption !== 1 ? 'disabled' : ''
-                    }`}
-                  >
-                    <label htmlFor="timeseries-logmode">Logarithmic</label>
-                    <input
-                      type="checkbox"
-                      checked={graphOption === 1 && timeseriesLogMode}
-                      className="switch"
-                      disabled={graphOption !== 1}
-                      onChange={(event) => {
-                        setTimeseriesLogMode(!timeseriesLogMode);
-                      }}
-                    />
-                  </div>
-                </div>
-
-                <div className="trends-state-name">
-                  <select
-                    onChange={({target}) => {
-                      onHighlightState(JSON.parse(target.value));
-                    }}
-                  >
-                    {states.map((s) => {
-                      return (
-                        <option
-                          key={s.statecode}
-                          value={JSON.stringify(s)}
-                          selected={s.statecode === activeStateCode}
-                        >
-                          {s.state === 'Total' ? 'All States' : s.state}
-                        </option>
-                      );
-                    })}
-                  </select>
-                </div>
-              </div>
-
-              <TimeSeries
-                timeseries={timeseries[activeStateCode]}
-                type={graphOption}
-                mode={timeseriesMode}
-                logMode={timeseriesLogMode}
-              />
+              
             </React.Fragment>
           )}
         </div>
